95ca690aad7f47e66d61232a5fa2365257ad11c2
Ramazan774 <ramazan.ramazanov@glion.ch>
2021-02-21 20:42:50 -0800

    m

--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,9 @@ class Timer extends Component {
   };
 
   // add your code here
+  componentWillUnmound(){
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
